423b54d967865d12e049038255d21f868169a586
hiokky <hiokky.work@gmail.com>
2024-11-11 10:25:32 +0900

    練習1

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,6 +91,7 @@ const IndexPage = () => (
       <p>練習C</p>
       <p>練習E</p>
       <p>練習D</p>
+      <p>練習1</p>
       <p className={styles.intro}>
         <b>Example pages:</b>{" "}
         {samplePageLinks.map((link, i) => (
